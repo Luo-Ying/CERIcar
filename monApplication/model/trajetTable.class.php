6ef3fb2719cbb2f1c2150acd374ba1f98ad26d52
Luo-Ying <luoyingqifr@gmail.com>
2021-10-29 02:06:07 +0200

    ---

---
+++ monApplication/model/trajetTable.class.php
@@ -0,0 +1,14 @@
+<?php
+
+// Inclusion de la classe trajet
+require_once "trajet.class.php";
+
+class trajetTable{
+
+    public static function getTrajet($depart, $arrivee){
+        
+    }
+
+}
+
+?>
